9eea46193827dbc984a4368801ed36fe51bf5bfa
Joanna E Kalema <kalemajoanna@gmail.com>
2019-10-28 19:24:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,13 +7,13 @@ end
 def subtraction(num1, num2)
 num1= 10 
 num2 = 5
-puts num1 - num2
+p num1 - num2
 end
 
 def division(num1, num2)
 num1=50
 num2=2 
-p num1/num2
+p num1 /num2
 end
 
 def multiplication(num1, num2)
